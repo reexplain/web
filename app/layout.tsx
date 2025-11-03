ae67a34a845af1ca80fef163e4a46733180ad7fa
Indrajit Kutuva <indrajitvijayakumar@gmail.com>
2025-11-03 12:33:36 +0530

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701&f[]=clash-grotesk@200,300,400,500,600,700&display=swap" rel="stylesheet" />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701&f[]=clash-grotesk@200,300,400,500,600,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicons/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="ReExplain" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </head>
-      <body
-        className={`p-4 lg:p-6 antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`p-4 lg:p-6 antialiased`}>{children}</body>
     </html>
   );
 }
